13ac2e84ddd3cc394f9f5ce52535ef59c04ce99d
MauricioMartiniJunior <martinijunior.mauricio@gmail.com>
2023-01-15 17:11:09 -0300

    usando cliboard-copy

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
+import clipboardCopy from "clipboard-copy";
 import { nanoid } from "nanoid";
 import './style.css';
+import copy from 'clipboard-copy';
 
 const button = document.querySelector('button');
 const display = document.querySelector('h2');
@@ -7,6 +9,11 @@ const display = document.querySelector('h2');
 button.addEventListener('click', () => {
     const password = nanoid();
     display.innerText = password;
+});
+
+button.addEventListener('click', (target) => {
+    copy(event.target.innerText);
+    alert('Senha copiada');
 });
 
 console.log(nanoid());
