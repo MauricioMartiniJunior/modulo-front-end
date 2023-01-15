805ccaa9fd003c4863910074c93f440df96f7b2b
MauricioMartiniJunior <martinijunior.mauricio@gmail.com>
2023-01-15 03:36:04 -0300

    adicionando a logica e eslint

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
 import { nanoid } from "nanoid";
- 
+
+const button = document.querySelector('button');
+const display = document.querySelector('h2');
+
+button.addEventListener('click', () => {
+    const password = nanoid();
+    display.innerText = password;
+
+});
 console.log(nanoid());
